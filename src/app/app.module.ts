4f3eb26b76f23779de9e503c8b8166f6e50fbedc
Daniel Sprohar <daniel.sprohar@outlook.com>
2022-03-19 10:17:19 -0500

    Added a topnav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './components/terminal/terminal.component';
+import { TopnavComponent } from './components/topnav/topnav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TerminalComponent
+    TerminalComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
