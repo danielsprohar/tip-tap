610c77f5e776fccd56a214e1c3074b615501220e
Daniel Sprohar <daniel.sprohar@outlook.com>
2022-03-20 01:02:46 -0500

    Fixed errors caused by the creation of an object

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Finger, Hand, Lesson, Level } from './models/lesson'
+
+export interface LessonData {
+  level: Level
+  hand: Hand
+  finger?: Finger
+  isHomeKeys?: boolean
+}
 
 @Component({
   selector: 'app-lessons',
   templateUrl: './lessons.component.html',
-  styleUrls: ['./lessons.component.scss']
+  styleUrls: ['./lessons.component.scss'],
 })
 export class LessonsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  createLesson(data: LessonData) {
+    return new Lesson({
+      level: data.level,
+      hand: data.hand,
+      finger: data.finger,
+      isHomeKeys: data.isHomeKeys,
+    })
   }
-
 }
