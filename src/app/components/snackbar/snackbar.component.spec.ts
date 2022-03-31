5b653ff2b6c5f30d39f14bcac753761b1cfb9e05
Daniel Sprohar <daniel.sprohar@outlook.com>
2022-03-31 17:39:52 -0500

    Tested the SnackbarComponent

--- src/app/components/snackbar/snackbar.component.spec.ts
+++ src/app/components/snackbar/snackbar.component.spec.ts
@@ -1,12 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { SnackbarService } from 'src/app/services/snackbar.service'
 import { SnackbarComponent } from './snackbar.component'
 
 describe('SnackbarComponent', () => {
   let component: SnackbarComponent
   let fixture: ComponentFixture<SnackbarComponent>
+  let snackbar: SnackbarService
+  let mockMessage: string
 
   beforeEach(async () => {
+    mockMessage = 'hello there'
+
     await TestBed.configureTestingModule({
       declarations: [SnackbarComponent],
     }).compileComponents()
@@ -14,11 +18,21 @@ describe('SnackbarComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SnackbarComponent)
+    snackbar = TestBed.inject(SnackbarService)
     component = fixture.componentInstance
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should display the message provided by the Snackbar service', (done: DoneFn) => {
+    snackbar.open(mockMessage)
+    fixture.detectChanges()
+    component.message$?.subscribe((message: string) => {
+      expect(message).toEqual(mockMessage)
+      done()
+    })
   })
 })
--- src/app/services/snackbar.service.ts
+++ src/app/services/snackbar.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core'
-import { Subject } from 'rxjs'
+import { BehaviorSubject, Subject } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class SnackbarService {
-  private readonly messageSubject = new Subject<string>()
+  private readonly messageSubject = new BehaviorSubject<string>('')
 
   constructor() {}
 
