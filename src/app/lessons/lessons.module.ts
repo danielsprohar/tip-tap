27ea106203d2821ebb0772925dfe5428439f3a52
Daniel Sprohar <daniel.sprohar@outlook.com>
2022-04-10 16:20:49 -0500

    Refactored the Lessons module

--- src/app/lessons/lessons.module.ts
+++ src/app/lessons/lessons.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { LessonsRoutingModule } from './lessons-routing.module';
-import { LessonsComponent } from './lessons.component';
-import { LessonDetailComponent } from './lesson-detail/lesson-detail.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
-
+import { LessonsRoutingModule } from './lessons-routing.module'
+import { LessonsComponent } from './lessons.component'
+import { LessonDetailComponent } from './lesson-detail/lesson-detail.component'
+import { SidenavComponent } from './sidenav/sidenav.component'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatDividerModule } from '@angular/material/divider'
 
 @NgModule({
-  declarations: [
-    LessonsComponent,
-    LessonDetailComponent,
-    SidenavComponent
-  ],
+  declarations: [LessonsComponent, LessonDetailComponent, SidenavComponent],
   imports: [
     CommonModule,
-    LessonsRoutingModule
-  ]
+    LessonsRoutingModule,
+    MatTabsModule,
+    MatDividerModule,
+  ],
 })
-export class LessonsModule { }
+export class LessonsModule {}
