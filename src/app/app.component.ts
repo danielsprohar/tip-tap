3f1b145645393b46985acbfbc73aa62d1cf2df20
Daniel Sprohar <daniel.sprohar@outlook.com>
2022-04-12 14:51:11 -0500

    Added a dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout'
+import { OverlayContainer } from '@angular/cdk/overlay'
 import { DOCUMENT } from '@angular/common'
-import { Component, Inject, OnInit } from '@angular/core'
+import {
+  Component,
+  HostBinding,
+  Inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core'
 import { AuthService } from '@auth0/auth0-angular'
-import { Observable, map, shareReplay } from 'rxjs'
+import { Observable, map, shareReplay, Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  isHandset$: Observable<boolean> = this.breakpointObserver
+export class AppComponent implements OnInit, OnDestroy {
+  private sub!: Subscription
+
+  isHandset$: Observable<boolean> = this.observer
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => result.matches),
@@ -18,12 +27,36 @@ export class AppComponent implements OnInit {
     )
 
   constructor(
-    private breakpointObserver: BreakpointObserver,
+    private observer: BreakpointObserver,
+    private overlay: OverlayContainer,
     public auth: AuthService,
     @Inject(DOCUMENT) public document: Document
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.sub = this.observer
+      .observe('(prefers-color-scheme: dark)')
+      .pipe(
+        map((result) => result.matches),
+        shareReplay()
+      )
+      .subscribe((isDarkTheme: boolean) => {
+        this.className = isDarkTheme ? 'dark-theme' : ''
+        if (isDarkTheme) {
+          this.overlay.getContainerElement().classList.add('dark-theme')
+        } else {
+          this.overlay.getContainerElement().classList.remove('dark-theme')
+        }
+      })
+  }
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe()
+    }
+  }
+
+  @HostBinding('class') className = ''
 
   logout() {
     this.auth.logout({ returnTo: document.location.origin })
